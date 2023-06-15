a38f2ffad056fc2700196103fef991998bcd15f0
Antônio Carlos <juniorbecari@gmail.com>
2023-06-15 17:58:03 -0300

    feat: adding parser

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,1 +1,18 @@
 mod ast;
+
+use crate::lexer::token::*;
+
+struct Parser {
+  input: Vec<Token>,
+  cursor: usize,
+}
+
+impl Parser {
+  fn new(input: Vec<Token>) -> Self {
+    Self { input: input, cursor: 0 }
+  }
+
+  fn advance(&mut self) {
+    self.cursor += 1;
+  }
+}
